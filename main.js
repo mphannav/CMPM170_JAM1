ea99959a101334a7c0babd7be1e9d71847f609c3
Michael Phannavong <mphannav@ucsc.edu>
2022-10-07 18:01:25 -0700

    updated and final

--- main.js
+++ main.js
@@ -34,9 +34,9 @@ function update() {
     nextPinDist = 5;
     cord = { angle: 1.57, length: cordLength, pin: pinMain[0] };
   }
-  let scr = 0.2;
+  let scr = 0.2 * difficulty;
   if (input.isPressed) {
-    cord.length += 2; // how fast it goes out
+    cord.length += 2 + difficulty; // how fast it goes out
     play("lazer");
     if(cord.length > 90)
     {
@@ -46,22 +46,27 @@ function update() {
   } else {
     cord.length += (cordLength - cord.length) * 0.2; //how fast it retracts
   }
+  color("black");
   //cord.angle += 0.05;
   line(cord.pin, vec(cord.pin).addWithAngle(cord.angle, cord.length));
-  if (cord.pin.y > 98) {
-    end();
-  }
   remove(pins, (p) => {
       //p.y  += scr;
       p.x  += scr;
+      color("red");
       box(p,2);
+      if (box(p, 2).isColliding.rect.black) {
+        play("explosion");
+        end();
+      }
+      return p.x > 102;
     });
     nextPinDist -= scr;
     while (nextPinDist < 0) {
       pins.push(vec(-2 - nextPinDist, rnd(10, 90)));
       nextPinDist += rnd(5, 50);
     }
-    color("light_black");
+    
+    color("light_blue");
     rect(0, 90, 100, 100);
 }
 addEventListener("load", onLoad);
